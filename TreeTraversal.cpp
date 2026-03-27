bd251075c71ea002ceff1a19f0ba92f22dc8a8f6
arbaj-silar <silararbaj.in>
2026-03-27 17:33:21 +0530

    level Order Traversal

--- TreeTraversal.cpp
+++ TreeTraversal.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node {
@@ -41,9 +42,7 @@ void inOrder(Node* root) {
     if(root == nullptr) return;
 
     inOrder(root->left);
-
     cout<<root->data<<" ";
-
     inOrder(root->right);
 }
 
@@ -55,10 +54,48 @@ void postOrder(Node* root) {
 
     cout<<root->data<<" ";
 }
+
+void levelOrder(Node* root) {
+    queue<Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while(q.size()>0) {
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr == NULL) {
+            if(!q.empty()) {
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        cout<<curr->data<<" ";
+
+        if(curr->left != NULL) {
+            q.push(curr->left);
+        }
+        if(curr->right != NULL) {
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
 int main() {
     vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node* root = buildTree(preOrder);
+    preOrderTraversal(root);
+    cout<<endl;
+    inOrder(root);
+    cout<<endl;
     postOrder(root);
+    cout<<endl;
+    levelOrder(root);
     return 0;
 }
