fd0b421daa77c5bb88142491aae02921017f37fd
arbaj-silar <silararbaj.in>
2026-03-28 10:01:52 +0530

    subTree search

--- IdenticalTree.cpp
+++ IdenticalTree.cpp
@@ -16,27 +16,35 @@ public:
 
 static int idx = -1;
 
-Node* buildTree(vector<int> &preOrder) {
+Node* buildTree(vector<int> &p) {
     idx++;
-    if(preOrder[idx] == -1) return NULL;
+    if(p[idx] == -1) return NULL;
 
-    Node* root = new Node(preOrder[idx]);
+    Node* root = new Node(p[idx]);
 
-    root->left = buildTree(preOrder);
-    root->right = buildTree(preOrder);
+    root->left = buildTree(p);
+    root->right = buildTree(p);
 
     return root;
 }
 
 bool identical(Node* r1, Node* r2) {
-    return identical(r1->left, r2->left) && identical(r1->right, r2->right) && r1->data == r2->data;
+    if(r1 == NULL || r2 == NULL) return r1 == r2;
+    return (identical(r1->left, r2->left) && identical(r1->right, r2->right) && r1->data == r2->data);
+}
+
+bool subTree(Node* r1, Node* r2) {
+    if(r1 == nullptr || r2 == nullptr) return r1 == r2;
+
+    if(r1->data == r2->data && identical(r1, r2)) return true;
+    return subTree(r1->left, r2) || subTree(r1->right , r2);
 }
 int main() {
     vector<int> p = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
-    vector<int> q = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node* r1 = buildTree(p);
-    Node* r2 = buildTree(q);
+    Node* r2 = buildTree(p);
     cout<<identical(r1, r2);
+    cout<<subTree(r1, r2);
     return 0;
 }
